e20b9ea4cc7777bd33982a9f35528eb430d61fe1
Vadym <vsamkovich@ukr.net>
2021-09-03 19:25:56 +0200

    creating batch service, which imports data from CSV to database

---
+++ src/main/java/com/example/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,50 @@
+package com.example.ipldashboard.data;
+
+import com.example.ipldashboard.model.Match;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+        Match match = new Match();
+        match.setId(Long.valueOf(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayerOfMatch());
+        match.setVenue(matchInput.getVenue());
+
+        //set team 1 and team 2 depending on the innings order
+        String firstInningsTeam, secondInningsTeam;
+
+        if("bat".equals(matchInput.getTossDecision())) {
+            firstInningsTeam = matchInput.getTossWinner();
+            secondInningsTeam = matchInput.getTossWinner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : match.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getTossWinner();
+            firstInningsTeam = matchInput.getTossWinner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : match.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+        match.setTossWinner(matchInput.getTossWinner());
+        match.setTossDecision(matchInput.getTossDecision());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResultMargin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        log.info("Converting (" + matchInput + ") into (" + match + ")");
+        return match;
+    }
+
+}
