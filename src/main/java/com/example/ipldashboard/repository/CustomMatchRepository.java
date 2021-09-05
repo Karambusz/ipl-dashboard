9f2238850bb8b1be9255721087b0ca45fc14a11e
Vadym <vsamkovich@ukr.net>
2021-09-05 18:47:23 +0200

    creaated team repository and team controller

---
+++ src/main/java/com/example/ipldashboard/repository/CustomMatchRepository.java
@@ -0,0 +1,19 @@
+package com.example.ipldashboard.repository;
+
+import com.example.ipldashboard.model.Match;
+import com.example.ipldashboard.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
+public interface CustomMatchRepository extends JpaRepository<Match, Long> {
+
+    @Query(value = "select m.team1, count(*) from Match m group by m.team1", nativeQuery = true)
+    List<String[]> findAllTeam1Matches();
+
+    @Query(value = "select m.team2, count(*) from Match m group by m.team2", nativeQuery = true)
+    List<String[]> findAllTeam2Matches();
+
+}
--- src/main/java/com/example/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/example/ipldashboard/repository/MatchRepository.java
@@ -1,18 +1,17 @@
 package com.example.ipldashboard.repository;
 
-import com.example.ipldashboard.model.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import com.example.ipldashboard.model.Match;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 
-public interface MatchRepository extends JpaRepository<Team, Long> {
+public interface MatchRepository extends CrudRepository<Match, Long> {
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 
-    @Query(value = "select m.team1, count(*) from Match m group by m.team1", nativeQuery = true)
-    List<String[]> findAllTeam1Matches();
-
-    @Query(value = "select m.team2, count(*) from Match m group by m.team2", nativeQuery = true)
-    List<String[]> findAllTeam2Matches();
-
+    default List<Match> findLatestMatchesByTeam(String teamName, int count) {
+        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
+    }
 }
