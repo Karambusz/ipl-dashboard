f0ae2efa96c6b2eda4abe81f2064fbfb14d72c78
Vadym <vsamkovich@ukr.net>
2021-09-09 17:55:58 +0200

    added styles for team page

--- src/frontend/src/components/match-detail-card/MatchDetailCard.js
+++ src/frontend/src/components/match-detail-card/MatchDetailCard.js
@@ -1,24 +1,39 @@
 import {React} from 'react';
 import {Link} from 'react-router-dom';
 
+import "./MatchDetailCard.scss";
+
 const MatchDetailCard = ({teamName, match}) => {
 	const {team1, team2, date, venue, matchWinner, resultMargin, result} = match;
 	const otherTeam = team1 === teamName ? team2 : team1;
 	const otherTeamRoute = `/teams/${otherTeam}`;
+	const isMatchWon = teamName === match.matchWinner;
+
 	if (!match) return null;
 
 
 	return (
-		<div className="match-detail-card">
-			<h3>Latest Matches</h3>
-			<h4>Match Details</h4>
-			<h1>vs <Link to={otherTeamRoute}>
-					{otherTeam}
-				</Link>
-			</h1>
-			<h2>{date}</h2>
-			<h3>at {venue}</h3>
-			<h3>{matchWinner} won by {resultMargin} {result}</h3>
+		<div className={isMatchWon ? "match-detail-card won-card" : "match-detail-card lost-card"}>
+			<div>
+				<span className="vs">vs</span>
+				<h1><Link to={otherTeamRoute}>
+						{otherTeam}
+					</Link>
+				</h1>
+				<h2 className="match-date">{date}</h2>
+				<h3 className="match-venue">at {venue}</h3>
+				<h3 className="match-result">{matchWinner} won by {resultMargin} {result}</h3>
+			</div>
+			<div className="additional-detail">
+				<h3>First Innings</h3>
+				<p>{match.team1}</p>
+				<h3>Second Innings</h3>
+				<p>{match.team2}</p>
+				<h3>Man of the match</h3>
+				<p>{match.playerOfMatch}</p>
+				<h3>Umpires</h3>
+				<p>{match.umpire1}, {match.umpire2}</p>
+			</div>
 		</div>
 	);
 }
--- src/frontend/src/components/match-small-card/MatchSmallCard.js
+++ src/frontend/src/components/match-small-card/MatchSmallCard.js
@@ -1,17 +1,22 @@
 import {React} from 'react';
 import {Link} from 'react-router-dom';
+import './MatchSmallCard.scss';
 
 const MatchSmallCard = ({match, teamName}) => {
 	const {matchWinner, resultMargin, result} = match;
 	const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
 	const otherTeamRoute = `/teams/${otherTeam}`;
+	const isMatchWon = teamName === match.matchWinner;
+
+
 	return (
-		<div className="match-small-card">
-			<h3>vs <Link to={otherTeamRoute}>
+		<div className= {isMatchWon ? "match-small-card won-card" : "match-small-card lost-card"}>
+			<span className="vs">vs</span>
+			<h3><Link to={otherTeamRoute}>
 					{otherTeam}
 				   </Link>
 			</h3>
-			<p>{matchWinner} won by {resultMargin} {result}</p>
+			<p className="match-result">{matchWinner} won by {resultMargin} {result}</p>
 		</div>
 	);
 }
